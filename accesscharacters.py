3c4ca6210ee6227682a32d5f1f17af62f7759c5a
RenuMagar <145140786+RenuMagar@users.noreply.github.com>
2023-09-17 15:22:44 +0530

    Create accesscharacters.py

---
+++ accesscharacters.py
@@ -0,0 +1,8 @@
+s="nutan"   #forwardmn=0 u=1 t=2 a=3 n= 4 #backword n=-1 a=-2 t=-3 u=-4 n=-5
+print(s[3]) #a
+print(s[-1]) #n
+
+# [start:end:step] 
+print(s[0:3]) #nut
+print(s[1:4:1]) #uta
+print(s[1:4:2]) #ua
