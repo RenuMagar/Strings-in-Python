24daa7a24e98ebf87241e84bb011db913978a1a5
RenuMagar <145140786+RenuMagar@users.noreply.github.com>
2023-09-17 15:26:48 +0530

    Create removespacesofstring.py

---
+++ removespacesofstring.py
@@ -0,0 +1,9 @@
+#removing spaces inside string 1.lstrip() 2.rstrip() 3.strip() 
+s="                   gayatri           "
+print(len(s))
+#y=s.lstrip() removing spaces from the string
+#print(len(y))
+#x=y.rstrip()
+#print(len(x))
+z=s.strip()
+print(len(z))
